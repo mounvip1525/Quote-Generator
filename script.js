4fedb37f6a58880654215fd701f9a9a20730a997
Your Name <you@example.com>
2020-09-06 20:21:28 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const quote=document.getElementById("quote");
+const quoteContainer=document.getElementById("quote-container");
+const author=document.getElementById("author");
+const whatsapp=document.getElementById("whatsapp");
+const facebook=document.getElementById("facebook");
+const instagram=document.getElementById("instagram");
+const next=document.getElementById("next")
+const loader=document.getElementById("loader");
+
+let quoteDetails=[];
+function getQuote(){
+
+  fetch('https://type.fit/api/quotes')
+  .then(function(res){
+    return res.json()
+  })
+  .then(function(data){
+    quoteDetails=data[Math.floor(Math.random() * data.length)];
+    quote.innerHTML= '"' + quoteDetails.text + '"' ;
+    if(quoteDetails.author === null)
+    author.innerHTML='-'+ "Unknown";
+    else
+    author.innerHTML='-'+ quoteDetails.author;
+
+  })
+  
+}
+ function whatsappmsg(){
+  window.open(`https://web.whatsapp.com//send?text=${quote}`);
+ 
+ }
+function instagrammsg(){
+window.open(`https://www.instagram.com`);
+}
+function facebookmsg(){
+  window.open(`https://facebook.com`);
+}
+next.addEventListener('click',getQuote);
+instagram.addEventListener('click',instagrammsg);
+facebook.addEventListener('click',facebookmsg);
+whatsapp.addEventListener('click',whatsappmsg);
+
+getQuote();
+
